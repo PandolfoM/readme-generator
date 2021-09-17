d12559e5dd6d7d523a9625081d68a79c5bba2fe8
Matthew Pandolfo <matt@pandolfo.com>
2021-09-17 15:19:37 -0400

    prompt 1st question

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+const readmeGenerator = (questions) => {
+  if (!questions) {
+    questions = [];
+  }
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your repository? (Required)",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter a title");
+          return false;
+        }
+      },
+    },
+  ])
+  .then((readmeData) => {
+    questions.push(readmeData)
+    return questions;
+  })
+};
 // Function call to initialize app
-init();
+readmeGenerator();
